4a68dfcb351f596f42afccc05f463f82e298f7af
Menqce <abdalla1202@hotmail.com>
2024-12-13 16:10:53 +0000

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,12 @@
 
+unit = input("Is this temprature in Celsius or Fahrenheit (C/F): ")
+temp = float(input("Enter the Temprature: "))
+
+if unit == "C":
+    temp = round((9 * temp) / 5 + 32, 1)
+    print(f"the Temprature in Fahrenheit is: {temp} °F")
+elif unit == "F":
+    temp = round((temp - 32) * 5 /9, 1)  
+    print(f"the temprature in Celsius is: {temp} °C")
+else:
+    print(f"{unit} Invalid action")    
