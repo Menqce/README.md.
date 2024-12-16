5032693df782ea15094eee2cb8c82bd8f60a2b0f
Menqce <abdalla1202@hotmail.com>
2024-12-16 12:00:58 +0000

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,1 +1,12 @@
+num = abs(int(input("Type the number ")))
+
+
+def factorial(number):
+    fact = number    
+    for i in range(number, 1, -1):
+        fact*=(i-1)
+    print(f'{number}:{fact}')
+        
+        
+factorial(num)
 
